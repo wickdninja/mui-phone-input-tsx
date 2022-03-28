58e66828a07f788c2facdef7eaa40b18dbda9c0a
Nate Ross <nate.ross@alliedpayment.com>
2022-03-28 13:14:58 -0400

    init commit

---
+++ phone-input.tsx
@@ -0,0 +1,40 @@
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
+// forked from https://github.com/andria-dev/mui-phone-input/blob/master/src/PhoneInput.js
+export default function PhoneInput({ ...props }) {
+  const [state, setState] = useState({
+    value: "",
+  });
+  return (
+    <TextField
+      inputProps={{ pattern: "[0-9]{3}-[0-9]{3}-[0-9]{4}" }}
+      type="tel"
+      {...props}
+      value={state.value}
+      onChange={(event) => {
+        const el = event.target;
+        let selectionStart = el.selectionStart;
+
+        const newInput = event.target.value.replace(/\D/g, "");
+
+        const oldValueArr = state.value.split("-");
+        const newValueArr = [
+          newInput.slice(0, 3),
+          newInput.slice(3, 6),
+          newInput.slice(6, 10),
+        ].filter((x) => x);
+
+        const newValue = newValueArr.join("-");
+
+        if (props.onChange) {
+          props.onChange(newValue);
+        }
+
+        setState({ value: newValue });
+        selectionStart += Math.max(newValueArr.length - oldValueArr.length, 0);
+        el.selectionStart = el.selectionEnd = selectionStart;
+      }}
+    />
+  );
+}
